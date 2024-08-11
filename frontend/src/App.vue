6abb2f0cbd330e297912dcdfb632cb07e4dae35d
dgikonyo <denniskimani05@gmail.com>
2024-08-11 21:06:56 +0300

    added homnepage skeleton

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,91 +1,82 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="We need change here" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <section class="hero-section">
+    <div class="left-hero">
+      <button class="btn btn-login">Login</button>
+      <button class="btn btn-sign-up">Sign Up</button>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="right-hero">
+      <div class="title">
+        <p class="title-text">konnect</p>
+      </div>
+
+      <div class="catchphrase">
+        <div class="phrase">
+          <p>Create.</p>
+        </div>
+        <div class="phrase">
+          <p>Ideas.</p>
+        </div>
+        <div class="phrase">
+          <p>Backers.</p>
+        </div>
+        <div class="phrase">
+          <p>Advertise.</p>
+        </div>
+      </div>
+
+      <div class="pointers">
+        <ul>
+          <li>
+            Crowdfunding platform for creatives, influencers and entrepreneurs
+          </li>
+          <li>Support/Funding Initiatives</li>
+          <li>Global Impact</li>
+        </ul>
+      </div>
+
+      <div class="cta">
+        <button class="btn campaign-button">Start a Campaign</button>
+        <button class="btn">Explore Projects</button>
+      </div>
+
+      <div class="partners">
+        <p>Collaborate with our partners</p>
+      </div>
+    </div>
+  </section>
+  <section class="campaigns-section">
+    <!-- verified projects -->
+    <div class="verified-projects">
+      <div class="section-description">
+        <p class="page-text">
+          We help local <span class="span-translucent">entrepreneurs</span> and
+          <span class="span-blue">organizations</span> raise funds for projects
+        </p>
+      </div>
+      <div class="projects">
+        <div class="project-card">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- projects near you -->
+    <div class="location-projects"></div>
+  </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
