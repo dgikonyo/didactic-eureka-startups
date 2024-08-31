59b1893ff3926f4ce191ccd95d839ae9120a4139
dgikonyo <denniskimani05@gmail.com>
2024-08-31 16:20:53 +0300

    homepage skeleton finished

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,63 +1,74 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import Footer from './components/FooterComponent.vue'
 </script>
 
 <template>
-  <section class="hero-section">
-    <div class="left-hero">
-      <button class="btn btn-login">Login</button>
-      <button class="btn btn-sign-up">Sign Up</button>
-    </div>
-
-    <div class="right-hero">
-      <div class="title">
-        <p class="title-text">konnect</p>
-      </div>
-
-      <div class="catchphrase">
-        <div class="phrase">
-          <p>Create.</p>
-        </div>
-        <div class="phrase">
-          <p>Ideas.</p>
-        </div>
-        <div class="phrase">
-          <p>Backers.</p>
-        </div>
-        <div class="phrase">
-          <p>Advertise.</p>
+  <section class="hero-section container">
+    <div class="row">
+      <div class="left-hero col-sm-12 col-md-12 col-lg-4">
+        <div class="row">
+          <button class="btn btn-login col-3">Login</button>
+          <button class="btn btn-sign-up col-3">Sign Up</button>
         </div>
       </div>
+      <div class="right-hero col">
+        <div class="title col-12">
+          <p class="title-text">konnect</p>
+        </div>
 
-      <div class="pointers">
-        <ul>
-          <li>
-            Crowdfunding platform for creatives, influencers and entrepreneurs
-          </li>
-          <li>Support/Funding Initiatives</li>
-          <li>Global Impact</li>
-        </ul>
-      </div>
+        <div class="catchphrase">
+          <div class="row">
+            <div class="phrase col-3">
+              <p>Create.</p>
+            </div>
+            <div class="phrase col-3">
+              <p>Ideas.</p>
+            </div>
+          </div>
 
-      <div class="cta">
-        <button class="btn campaign-button">Start a Campaign</button>
-        <button class="btn">Explore Projects</button>
-      </div>
+          <div class="row">
+            <div class="phrase col-3">
+              <p>Backers.</p>
+            </div>
+            <div class="phrase col-3">
+              <p>Advertise.</p>
+            </div>
+          </div>
+        </div>
 
-      <div class="partners">
-        <p>Collaborate with our partners</p>
+        <div class="pointers">
+          <ul>
+            <li>
+              Crowdfunding platform for creatives, influencers and entrepreneurs
+            </li>
+            <li>Support/Funding Initiatives</li>
+            <li>Global Impact</li>
+          </ul>
+        </div>
+
+        <div class="cta row">
+          <button class="btn campaign-button col-sm-12 col-md-12 col-lg-6">Start a Campaign</button>
+          <button class="btn col-sm-12 col-md-12 col-lg-6">Explore Projects</button>
+        </div>
+
+        <div class="partners">
+          <p>Collaborate with our partners</p>
+        </div>
       </div>
     </div>
   </section>
   <section class="campaigns-section">
     <!-- verified projects -->
     <div class="verified-projects">
-      <div class="section-description">
-        <p class="page-text">
+      <div class="section-description row">
+        <p class="page-text col-8">
           We help local <span class="span-translucent">entrepreneurs</span> and
           <span class="span-blue">organizations</span> raise funds for projects
         </p>
+        <a class="col-4">View All</a>
       </div>
+
       <div class="projects">
         <div class="project-card">
           <div class="card" style="width: 18rem;">
@@ -76,6 +87,9 @@ import { RouterLink, RouterView } from 'vue-router';
     </div>
     <!-- projects near you -->
     <div class="location-projects"></div>
+  </section>
+  <section class="footer">
+    <Footer></Footer>
   </section>
 </template>
 
---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="footer-section container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <p>Konnect</p>
+                <p>We have been connecting inventors with investors since 2021</p>
+                <p>Follow us</p>
+                <ul>
+                    <li>Twitter</li>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <p>Let us Help You</p>
+                <ul>
+                    <li>Contact Us</li>
+                    <li>About Us</li>
+                    <li>Services</li>
+                    <li>Blog</li>
+                    <li>Career</li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <p>Policies</p>
+                <ul>
+                    <li>Cookies Policy</li>
+                    <li>Privacy Notice</li>
+                    <li>Terms and Conditions</li>
+                    <li>Report</li>
+                    <li>How do we use your data</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped></style>
