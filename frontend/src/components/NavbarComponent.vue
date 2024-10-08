7ae6e6e2e9458c2c5ba49626a2a7ccd5222c7192
dgikonyo <denniskimani05@gmail.com>
2024-10-09 00:12:46 +0300

    added styling for class

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -6,36 +6,53 @@
         <div class="navigation-bar">
             <div class="container">
                 <div class="nav-items row">
-                    <div class="button-div col-sm-12 col-md-3 col-lg-3">
-                        <button class="btn btn-login col-sm-6 col-md col-lg">Login</button>
-                        <button class="btn btn-sign-up col-sm-6 col-md col-lg">Sign Up</button>
+                    <div class="nav-item col-sm-12 col-md-4 col-lg-4">
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <button class="btn btn-login">Login</button>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <button class="btn btn-sign-up">Sign Up</button>
+                        </div>
                     </div>
-                    <div class="title-div col-sm-12 col-md-3 col-lg-3">
-                        <p class="title">Konnect</p>
+                    <div class="nav-item col-sm-12 col-md-4 col-lg-4">
+                        <RouterLink to='/' class="title">Konnect</RouterLink>
                     </div>
-                    <div class="explore-div col-sm-12 col-md-3 col-lg-3">
-                        <RouterLink class="explore" to="/campaign/start/questions">Explore Projects <span> <font-awesome-icon :icon="['fas', 'right-long']" /></span></RouterLink>
+                    <div class="nav-item col-sm-12 col-md-4 col-lg-4">
+                        <RouterLink class="explore" to="/campaign/start/questions">Explore Projects <span> <font-awesome-icon :icon="['fas', 'arrow-right']" size="xs" class="explore-icon" /></span></RouterLink>
                     </div>
                 </div>
             </div> 
+            <div class="nav-line"></div>
         </div>
     </section>
 </template>
 <style scoped>
     .navigation-bar {
+        width: 100%;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        margin-bottom: 20px;
+    }
+    .nav-items {
         display: flex;
-        justify-content: space-between;
-        align-content: center;
-        flex-wrap: nowrap;
-        width: 100%;    
+        justify-content: center;
+        align-items: center;
+    }
+    .nav-item {
+        display: flex;
+        justify-content: center;
+    }
+
+    a {
+        text-decoration: none;
     }
 
     .btn {
-        font-size: 18px;
+        font-size: 16px;
         font-family: 'Inter', sans-serif;
         border-radius: 8px;
-        padding: 5px 10px;
-        margin-right: 5px;
+        padding: 5px 5px;
+        margin-right:10px;
+        width: 100px;
     }
 
     .btn-login {
@@ -46,9 +63,23 @@
 
     .btn-sign-up {
         background-color: hsla(0, 0%, 100%, 0.3);
-        font-size: 18px;
-        font-family: 'Inter', sans-serif;
         color: black;
         border: black, 0.8px, solid;
+    }
+
+    .title {
+        font-size: 30px;
+        color:black;
+        font-weight: 500;
+    }
+
+    .explore {
+        font-size: 20px;
+        color:black;
+        font-weight: 500;
+    }
+
+    .explore-icon {
+        margin:0 5px 0 5px;
     }
 </style>
