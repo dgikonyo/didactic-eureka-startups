2c38ace78f718e240e2dbc4e0cc9800808a5ff52
Kimani <KEN22934@kcbgroup.com>
2024-10-31 15:52:21 +0300

    content page skeleton done

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { RouterLink, RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 <template>
     <section class="navbar-section">
@@ -18,68 +18,75 @@
                         <RouterLink to='/' class="title">Konnect</RouterLink>
                     </div>
                     <div class="nav-item col-sm-12 col-md-4 col-lg-4">
-                        <RouterLink class="explore" to="/campaign/questions">Explore Projects <span> <font-awesome-icon :icon="['fas', 'arrow-right']" size="xs" class="explore-icon" /></span></RouterLink>
+                        <RouterLink class="explore" to="/campaign/questions">Explore Projects <span> <font-awesome-icon
+                                    :icon="['fas', 'arrow-right']" size="xs" class="explore-icon" /></span></RouterLink>
                     </div>
                 </div>
-            </div> 
+            </div>
             <div class="nav-line"></div>
         </div>
     </section>
 </template>
 <style scoped>
-    .navigation-bar {
-        width: 100%;
-        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-        margin-bottom: 20px;
-    }
-    .nav-items {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .nav-item {
-        display: flex;
-        justify-content: center;
-    }
+.navbar-section {
+    max-width: 1280px;
+}
 
-    a {
-        text-decoration: none;
-    }
+.navigation-bar {
+    width: 100%;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    margin-bottom: 20px;
+}
 
-    .btn {
-        font-size: 16px;
-        font-family: 'Inter', sans-serif;
-        border-radius: 8px;
-        padding: 5px 5px;
-        margin-right:10px;
-        width: 100px;
-    }
+.nav-items {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
-    .btn-login {
-        background-color: hsla(333, 100%, 45%, 1);
-        color: white;
-        border: none;
-    }
+.nav-item {
+    display: flex;
+    justify-content: center;
+}
 
-    .btn-sign-up {
-        background-color: hsla(0, 0%, 100%, 0.3);
-        color: black;
-        border: black, 0.8px, solid;
-    }
+a {
+    text-decoration: none;
+}
 
-    .title {
-        font-size: 30px;
-        color:black;
-        font-weight: 500;
-    }
+.btn {
+    font-size: 16px;
+    font-family: 'Inter', sans-serif;
+    border-radius: 8px;
+    padding: 5px 5px;
+    margin-right: 10px;
+    width: 100px;
+}
 
-    .explore {
-        font-size: 20px;
-        color:black;
-        font-weight: 500;
-    }
+.btn-login {
+    background-color: hsla(333, 100%, 45%, 1);
+    color: white;
+    border: none;
+}
 
-    .explore-icon {
-        margin:0 5px 0 5px;
-    }
+.btn-sign-up {
+    background-color: hsla(0, 0%, 100%, 0.3);
+    color: black;
+    border: black, 0.8px, solid;
+}
+
+.title {
+    font-size: 30px;
+    color: black;
+    font-weight: 500;
+}
+
+.explore {
+    font-size: 20px;
+    color: black;
+    font-weight: 500;
+}
+
+.explore-icon {
+    margin: 0 5px 0 5px;
+}
 </style>
