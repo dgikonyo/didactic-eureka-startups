3c4770cb33821a2ee71ca0f48d5b3181ae0e29a2
Kimani <KEN22934@kcbgroup.com>
2024-11-21 16:09:38 +0300

    categories added

--- frontend/src/services/CountryService.ts
+++
@@ -1,9 +0,0 @@
-import type { Country } from '@/types/Country';
-import axios from 'axios';
-
-const BACKEND_URL = '';
-
-export const getCountries = async (): Promise<Country[]> => {
-  const response = await axios.get('BACKEND_URL/countries');
-  return response.data;
-};
