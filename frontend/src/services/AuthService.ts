7331057be99abdbca3fb3ec46cf0119944820970
dgikonyo <denniskimani05@gmail.com>
2025-01-17 23:58:22 +0300

    authentication complete

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import type { User } from '@/types/User';
 import type { LoginDto } from '@/types/User';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+import { useRouter } from 'vue-router';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const JWT_SECRET = import.meta.env.VITE_SECRET_KEY;
 const JWT_ALGORITHM = import.meta.env.VITE_JWT_ALGORITHM;
+const router = useRouter();
 
 export class AuthService {
   async registerUser(user: User): Promise<any> {
     try {
-      const response = await axios({
-        method:'POST',
-        url:`${BACKEND_URL}/auth/register/users`,
-        data: user
-      });
+      const response = await axios.post(`${BACKEND_URL}/auth/register/users`, user);
+      
+      console.log(response.data);
+      router.push('/sign-in');
 
       return response.data;
     } catch (error) {
@@ -24,18 +25,15 @@ export class AuthService {
 
   async loginInUser(payload: LoginDto): Promise<any> {
     try {
-      const response = await axios({
-        method:'POST',
-        url:`${BACKEND_URL}/auth/sign-in`,
-        data: payload
-      }).then(res => {
+      const response = await axios.post(`${BACKEND_URL}/auth/sign-in`, payload
+      ).then(res => {
         if (res.data.accessToken) {
           localStorage.setItem('user', JSON.stringify(res.data));
         }
 
         return res;
       });
-      
+
       return response.data;
     } catch (error) {
       console.error('Login failed', error);
@@ -45,9 +43,5 @@ export class AuthService {
 
   async logout() {
     localStorage.removeItem('user');
-  }
-
-  private async decodeToken(token: any) {
-    return jwtDecode(token);
   }
 }
