1c453303ee94e80c59aad3811c9af04ca20bdc84
dgikonyo <denniskimani05@gmail.com>
2025-02-18 20:05:59 +0300

    eh

---
+++ frontend/src/components/CampaignCard.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import '../assets/main.css';
+	/*
+load the card with campaign information
+	*/
+
+export default {
+	name:'CampaignCard'
+};
+</script>
+<template>
+    <div class="card col-sm-12 col-md-4 col-lg-4">
+        <div class="pic-space">
+            <img :src="imgrl" class="card-img-top" alt="..." />
+        </div>
+        <div class="card-body">
+            <h5 class="category">Some details</h5>
+            <p class="card-text">
+               some text
+            </p>
+            <p class="card-text">Campaign target <span class="price">some amount</span></p>
+        </div>
+        <div class="campaign-button">
+        	<button class="btn-campaign">Click</button>
+        </div>
+    </div>
+</template>
--- frontend/src/views/profile/ProfileView.vue
+++ frontend/src/views/profile/ProfileView.vue
@@ -2,9 +2,16 @@
 import { useAuthStore } from '@/stores';
 import { useRouter } from 'vue-router';
 import '../../assets/main.css';
+import CampaignCard from '@/components/CampaignCard.vue';
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import type {User} from '@/types/User.ts';
 
 export default {
   name: 'profile',
+  components: {
+    CampaignCard,
+    NavbarComponent
+  },
   setup() {
     const authStore = useAuthStore();
     const router = useRouter();
@@ -13,15 +20,17 @@ export default {
       router.push('/sign-in');
     }
 
-	
+  const isAuthenticated = authStore.user;
     return {
-			
+			isAuthenticated
     };
   },
 };
 </script>
 <template>
 	<section class="profile-section">
-		
+     <NavbarComponent></NavbarComponent>
+		<CampaignCard/>
+    {{isAuthenticated}}
 	</section>
 </template>
